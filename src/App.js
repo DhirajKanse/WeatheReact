ff53ece192ff992759a054019c3c2aad2f02c9f2
DhirajKanse <dhirajkanse007@gmail.com>
2024-03-17 21:44:25 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import './App.css';
+import WeatherApp from './Components/WeatherApp/WeatherApp';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <WeatherApp/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
